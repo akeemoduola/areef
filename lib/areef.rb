f4d4fe55af612570377aa9fe553a5678ffabef2d
Oduola Akeem <akeem.oduola@andela.com>
2016-02-25 02:36:28 +0100

    Setup a skeletal Application to test Areef gem and add first set of tests. [Finishes #114449675]

--- lib/areef.rb
+++ lib/areef.rb
@@ -1,20 +1,23 @@
 require "areef/version"
 
 module Areef
-  def call(env)
+  class Application
+    def call(env)
       @req = Rack::Request.new(env)
       path = @req.path_info
       request_method = @req.request_method.downcase
       return [500, {}, []] if path == "/favicon.ico"
       controller_class, action = get_controller_and_action(path, request_method)
       response = controller_class.new.send(action)
-      [200, {"Content-Type" => "text/html"}, [response]]
+      [200, { "Content-Type" => "text/html" }, [response]]
     end
 
     def get_controller_and_action(path, verb)
       _, controller_name, action = path.split("/")
+      require "#{controller_name.downcase}_controller.rb"
       controller_name = Object.const_get(controller_name.capitalize! + "Controller")
       action = action.nil? ? verb : "#{verb}_#{action}"
       [ controller_name, action ]
     end
+  end
 end
