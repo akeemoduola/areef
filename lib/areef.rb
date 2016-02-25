38b81e1abb6adb87fe6ade918df32452acdc2dab
Oduola Akeem <akeem.oduola@andela.com>
2016-02-25 01:33:15 +0100

    Create a basic application class, set up testing and CI [Finishes #114447521]

--- lib/areef.rb
+++ lib/areef.rb
@@ -1,5 +1,20 @@
 require "areef/version"
 
 module Areef
-  # Your code goes here...
+  def call(env)
+      @req = Rack::Request.new(env)
+      path = @req.path_info
+      request_method = @req.request_method.downcase
+      return [500, {}, []] if path == "/favicon.ico"
+      controller_class, action = get_controller_and_action(path, request_method)
+      response = controller_class.new.send(action)
+      [200, {"Content-Type" => "text/html"}, [response]]
+    end
+
+    def get_controller_and_action(path, verb)
+      _, controller_name, action = path.split("/")
+      controller_name = Object.const_get(controller_name.capitalize! + "Controller")
+      action = action.nil? ? verb : "#{verb}_#{action}"
+      [ controller_name, action ]
+    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require "coveralls"
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'areef'
+require 'rspec'
+require 'rack/test'
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
