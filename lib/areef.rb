d388b055c112b41c542cf85e6861bf285d2effae
Oduola Akeem <akeem.oduola@andela.com>
2016-02-25 15:05:43 +0100

    update application class to use new routing scheme

--- lib/areef.rb
+++ lib/areef.rb
@@ -2,25 +2,3 @@ require "areef/version"
 require "areef/utility"
 require "areef/dependencies"
 require "areef/routing/router"
-module Areef
-  class Application
-    def call(env)
-      @req = Rack::Request.new(env)
-      path = @req.path_info
-      request_method = @req.request_method.downcase
-      return [500, {}, []] if path == "/favicon.ico"
-      controller_class, action = get_controller_and_action(path, request_method)
-      response = controller_class.new.send(action)
-      [200, { "Content-Type" => "text/html" }, [response]]
-    end
-
-    def get_controller_and_action(path, verb)
-      _, controller_name, action = path.split("/")
-      require "#{controller_name.downcase}_controller.rb"
-      controller_name = Object.const_get(controller_name.
-                               capitalize! + "Controller")
-      action = action.nil? ? verb : "#{verb}_#{action}"
-      [controller_name, action]
-    end
-  end
-end
